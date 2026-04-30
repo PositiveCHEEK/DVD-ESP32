32827efa2327661b729f4018b8066c7d5bf5813c
PositiveCHEEK <164353842+PositiveCHEEK@users.noreply.github.com>
2026-04-30 23:01:48 +0300

    Super dvd.cpp hitbox update!!
    added hitboxez.

--- dvd.cpp
+++ dvd.cpp
@@ -8,7 +8,8 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-std::vector<int> cache = {10, 1};
+std::vector<int> pos1 = {10, 1};
+std::vector<int> pos2 = {21, 8};
 int direction = 0;
 
 void setup() {
@@ -20,7 +21,7 @@ void setup() {
 void loop() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  if (cache[0] == SCREEN_WIDTH) { // Main check!!
+  if (pos1[0] + pos2[0] == SCREEN_WIDTH) { // Main check!!
     switch (direction) {
       case 0:
         direction = 3;
@@ -30,7 +31,7 @@ void loop() {
         break;
     }
   }
-  if (cache[0] == 0) {
+  if (pos1[0] == 0) {
     switch (direction) {
       case 3:
         direction = 0;
@@ -40,7 +41,7 @@ void loop() {
         break;
     }
   }
-  if (cache[1] == SCREEN_HEIGHT) {
+  if (pos1[1] + pos2[1] == SCREEN_HEIGHT) {
     switch (direction) {
       case 3:
         direction = 2;
@@ -50,7 +51,7 @@ void loop() {
         break;
     }
   }
-  if (cache[1] == 0) {
+  if (pos1[1] == 0) {
     switch (direction) {
       case 2:
         direction = 3;
@@ -63,25 +64,25 @@ void loop() {
 
   switch (direction) { // Processing of DR-UR-UL-DL
     case 0:
-      cache[0] += 1;
-      cache[1] += 1;
+      pos1[0] += 1;
+      pos1[1] += 1;
       break;
     case 1:
-      cache[0] += 1;
-      cache[1] -= 1;
+      pos1[0] += 1;
+      pos1[1] -= 1;
       break;
     case 2:
-      cache[0] -= 1;
-      cache[1] -= 1;
+      pos1[0] -= 1;
+      pos1[1] -= 1;
       break;
     case 3:
-      cache[0] -= 1;
-      cache[1] += 1;
+      pos1[0] -= 1;
+      pos1[1] += 1;
       break;
   } // Processing of ts not
-  display.setCursor(cache[0], cache[1]);
-  display.println("DVD"); // Change this to wtvr you like
+  display.setCursor(pos1[0], pos1[1]);
+  display.println("DVD");
   display.display();
-  delay(40); // FPS
+  delay(40);
   display.clearDisplay();
 }
